d68262d9abe261626be9e91a1aaa4013752ff98a
Breck Zeyer <breckzeyer@Brecks-Air.attlocal.net>
2021-09-26 15:57:06 -0500

    trying to center text on buttons

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -6,7 +6,7 @@ function SearchBar(props) {
       <input
         className="search-bar-input"
         type="search"
-        placeholder="search"
+        placeholder="search item name"
         value={props.searchName}
         onChange={(e) => props.setSearchName(e.target.value)}
       />
