be8281da392ddb3338c02e0a4bb726b6cf5cdaa2
Breck Zeyer <breckzeyer@Brecks-Air.attlocal.net>
2021-09-26 21:11:25 -0500

    used react-icons to make buttons

--- client/src/components/ListItems.jsx
+++ client/src/components/ListItems.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import { changeQuantity, deleteItem, getItems } from "../services/API";
 import "./ListItems.css";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faMinus} from "@fortawesome/free-solid-svg-icons";
-// import { faPlus} from "@fortawesome/free-solid-svg-icons";
+import { HiOutlinePlusCircle } from 'react-icons/hi';
+import { HiOutlineMinusCircle } from 'react-icons/hi';
+import { HiOutlineXCircle } from 'react-icons/hi';
 
 function ListItems(props) {
   const [itemNames, setItemNames] = useState([]);
@@ -55,38 +55,24 @@ function ListItems(props) {
         {itemNames.map((item) => {
           return (
             <div className="listitems-display" key={item.id}>
-              <button
-                className="display-button decrement"
-                onClick={() => {
+              <HiOutlineMinusCircle className="display-button decrement" onClick={() => {
                   handleDecrement(item);
-                }}
-              >
-                <div className="horizontal-decrement"></div>
-                {/* &#8722; */}
-              </button>
+                }}/>
+              
               <span className="quantity-display"> {item.fields.quantity} </span>
-              <button
-                className="display-button increment"
-                onClick={() => {
-                  handleIncrement(item);
-                }}
-              >
-                <div className="horizontal-increment"></div>
-                <div className="vertical-increment"></div>
-                {/* &#43; */}
-              </button>
-              {/* <FontAwesomeIcon onClick={() => { handleIncrement(item) }} className="increment" icon={faPlus} size="sm" /> */}
+              
+                
+                <HiOutlinePlusCircle
+                  className="display-button increment"
+                  onClick={() => {
+                    handleIncrement(item);
+                  }}
+                />
               <span className="uofm-display"> {item.fields.uofm} </span>
               <span className="food-name"> {item.fields.name} </span>
               <span className="location-display"> {item.fields.location} </span>
-              <button
-                className="display-button delete"
-                onClick={() => handleDelete(item.id)}
-              >
-                {/* <div className="horizontal-delete"></div>
-                <div className="vertical-delete"></div> */}
-                x
-              </button>
+              <HiOutlineXCircle className="display-button delete" onClick={() => handleDelete(item.id)}/>
+              
             </div>
           );
         })}
